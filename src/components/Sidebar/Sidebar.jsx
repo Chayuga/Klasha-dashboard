bbec4562e93a0d9b2bacb0db5aaa5fad50c718df
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-15 21:07:25 +0300

    fix: fix table sidebar on responsive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ import klashaFullLogo from '../../assets/logo/klasha__logo.svg';
 import klashaKLogo from '../../assets/logo/klasha-k.svg';
 import { useStateContext } from '../../contexts/ContextProvider';
 
-const SidebarMenu = () => {
+const Sidebar = () => {
   const { fullMenu, setFullMenu } = useStateContext();
 
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -21,91 +21,93 @@ const SidebarMenu = () => {
   const normalLink =
     'flex items-center  pl-4 pt-3 pb-2.5 text-md text-black hover:text-gray-500';
 
-  return (
-    <div>
-      <div className='flex flex-col items-center mt-8 w-'>
-        {mobileMenu && (
-          <button type='button' onClick={() => setMobileMenu(true)}>
-            <MdOutlineCancel />
-          </button>
-        )}
-        <Link to='/' className='flex justify-center item-center gap-3 mt-4 '>
-          {fullMenu ? (
-            <img src={klashaFullLogo} alt='klasha logo' />
-          ) : (
-            <img src={klashaKLogo} alt='klasha logo' />
-          )}
-        </Link>
-      </div>
-      <div className='flex flex-col items-center '>
-        <div className='mt-10 '>
-          {links.map((item) => (
-            <div key={item.title}>
-              <p className='text-gray-400 my-3 mt-4 '>
-                {fullMenu ? `${item.title1}` : `${item.title2}`}
-              </p>
-              {item.links.map((link) => (
-                <NavLink
-                  to={link.path ? `/${link.path}` : '#'}
-                  key={link.name}
-                  className={({ isActive }) => [
-                    isActive ? activeLink : normalLink,
-                    !link.path && normalLink,
-                  ]}
-                >
-                  {fullMenu ? (
-                    <div
-                      className={`flex items-center {({ isActive }) => [
-                            isActive ? activeLink : normalLink,
-                          ]}`}
-                    >
-                      <p className='mr-2'>{link.iconActive}</p>
-                      <p>{link.name}</p>
-                    </div>
-                  ) : (
-                    <div
-                      className={`flex items-center {({ isActive }) => [
-                            isActive ? activeLink : normalLink,
-                          ]}`}
-                    >
-                      <p className='mr-2'>{link.iconActive}</p>
-                    </div>
-                  )}
-                </NavLink>
-              ))}
-            </div>
-          ))}
-        </div>
-        {/* lower hide button */}
-        <button
-          type='button'
-          onClick={() => setFullMenu((prevActiveMenu) => !prevActiveMenu)}
-          className='flex items-center justify-center outline rounded-md mx-5 p-3 hover:bg-light-gray mt-20'
-        >
-          {fullMenu ? (
-            <>
-              <AiOutlineLeft />
-              <span className='ml-2'>Hide panel</span>
-            </>
-          ) : (
-            <>
-              <AiOutlineRight />
-            </>
-          )}
-        </button>
-      </div>
-    </div>
-  );
-};
-
-const Sidebar = () => {
   const mobile = useMediaQuery('(max-width:640px)');
+  const tablet = useMediaQuery('(max-width:1024px)');
+
+  if (tablet === true) {
+    setFullMenu(false);
+  }
 
   return (
     <>
       {!mobile && (
         <div className='h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto bg-sidebar-bg'>
-          <SidebarMenu />
+          <div>
+            <div className='flex flex-col items-center mt-8 w-'>
+              {mobileMenu && (
+                <button type='button' onClick={() => setMobileMenu(true)}>
+                  <MdOutlineCancel />
+                </button>
+              )}
+              <Link
+                to='/'
+                className='flex justify-center item-center gap-3 mt-4 '
+              >
+                {fullMenu ? (
+                  <img src={klashaFullLogo} alt='klasha logo' />
+                ) : (
+                  <img src={klashaKLogo} alt='klasha logo' />
+                )}
+              </Link>
+            </div>
+            <div className='flex flex-col items-center '>
+              <div className='mt-10 '>
+                {links.map((item) => (
+                  <div key={item.title}>
+                    <p className='text-gray-400 my-3 mt-4 '>
+                      {fullMenu ? `${item.title1}` : `${item.title2}`}
+                    </p>
+                    {item.links.map((link) => (
+                      <NavLink
+                        to={link.path ? `/${link.path}` : '#'}
+                        key={link.name}
+                        className={({ isActive }) => [
+                          isActive ? activeLink : normalLink,
+                          !link.path && normalLink,
+                        ]}
+                      >
+                        {fullMenu ? (
+                          <div
+                            className={`flex items-center {({ isActive }) => [
+                            isActive ? activeLink : normalLink,
+                          ]}`}
+                          >
+                            <p className='mr-2'>{link.iconActive}</p>
+                            <p>{link.name}</p>
+                          </div>
+                        ) : (
+                          <div
+                            className={`flex items-center {({ isActive }) => [
+                            isActive ? activeLink : normalLink,
+                          ]}`}
+                          >
+                            <p className='mr-2'>{link.iconActive}</p>
+                          </div>
+                        )}
+                      </NavLink>
+                    ))}
+                  </div>
+                ))}
+              </div>
+              {/* lower hide button */}
+              <button
+                type='button'
+                onClick={() => setFullMenu((prevActiveMenu) => !prevActiveMenu)}
+                className='flex items-center justify-center outline rounded-md mx-5 p-3 hover:bg-light-gray mt-20'
+              >
+                {fullMenu ? (
+                  <>
+                    <AiOutlineLeft />
+                    <span className='ml-2'>Hide panel</span>
+                  </>
+                ) : (
+                  <>
+                    <AiOutlineRight />
+                  </>
+                )}
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </>
