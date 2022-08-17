7950efb3d1e9d5275ed598c505566b5fc95dfaa8
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-18 02:56:21 +0300

    fix: build errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useStateContext } from './contexts/ContextProvider';
 
 import { Navbar, Sidebar } from './components';
